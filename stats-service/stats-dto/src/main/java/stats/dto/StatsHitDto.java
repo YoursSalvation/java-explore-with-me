b1e8d9ce5ab681020e3c6fa410ba29b0cf2da52b
Dmitry Prokhorov <prokhorovds26@mail.ru>
2026-01-14 01:55:46 +0500

    stats-dto: pom.xml: add jackson.annotation dependency. StatsHitDto: add @JsonFormat to timestamp

--- stats-service/stats-dto/src/main/java/stats/dto/StatsHitDto.java
+++ stats-service/stats-dto/src/main/java/stats/dto/StatsHitDto.java
@@ -1,5 +1,6 @@
 package stats.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
@@ -19,6 +20,6 @@ public class StatsHitDto {
     private String ip;
 
     @NotNull
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime timestamp;
-
 }
