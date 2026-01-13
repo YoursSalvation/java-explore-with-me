bf14c0bc4b57bcfa45d04f9b9488bd9a8c97f497
Dmitry Prokhorov <prokhorovds26@mail.ru>
2026-01-14 02:05:15 +0500

    stats-server: HitRepository: add ORDER BY COUNT

--- stats-service/stats-server/src/main/java/stats/server/repository/HitRepository.java
+++ stats-service/stats-server/src/main/java/stats/server/repository/HitRepository.java
@@ -1,9 +1,9 @@
 package stats.server.repository;
 
-import stats.dto.StatsViewDto;
-import stats.server.model.Hit;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import stats.dto.StatsViewDto;
+import stats.server.model.Hit;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -11,16 +11,17 @@ import java.util.List;
 public interface HitRepository extends JpaRepository<Hit, Long> {
 
     @Query("""
-        SELECT new stats.dto.StatsViewDto(
-            h.app,
-            h.uri,
-            COUNT(DISTINCT h.ip)
-        )
-        FROM Hit h
-        WHERE h.timestamp BETWEEN :start AND :end
-        AND (:uris IS NULL OR h.uri IN :uris)
-        GROUP BY h.app, h.uri
-    """)
+                SELECT new stats.dto.StatsViewDto(
+                    h.app,
+                    h.uri,
+                    COUNT(DISTINCT h.ip)
+                )
+                FROM Hit h
+                WHERE h.timestamp BETWEEN :start AND :end
+                  AND (:uris IS NULL OR h.uri IN :uris)
+                GROUP BY h.app, h.uri
+                ORDER BY COUNT(DISTINCT h.ip) DESC
+            """)
     List<StatsViewDto> findUniqueStats(
             LocalDateTime start,
             LocalDateTime end,
@@ -28,16 +29,17 @@ public interface HitRepository extends JpaRepository<Hit, Long> {
     );
 
     @Query("""
-        SELECT new stats.dto.StatsViewDto(
-            h.app,
-            h.uri,
-            COUNT(h.ip)
-        )
-        FROM Hit h
-        WHERE h.timestamp BETWEEN :start AND :end
-        AND (:uris IS NULL OR h.uri IN :uris)
-        GROUP BY h.app, h.uri
-    """)
+                SELECT new stats.dto.StatsViewDto(
+                    h.app,
+                    h.uri,
+                    COUNT(h.id)
+                )
+                FROM Hit h
+                WHERE h.timestamp BETWEEN :start AND :end
+                  AND (:uris IS NULL OR h.uri IN :uris)
+                GROUP BY h.app, h.uri
+                ORDER BY COUNT(h.id) DESC
+            """)
     List<StatsViewDto> findAllStats(
             LocalDateTime start,
             LocalDateTime end,
