a646577bae6f8e61e9e059f32c24f908616d102d
Dmitry Prokhorov <prokhorovds26@mail.ru>
2026-01-19 22:38:24 +0500

    some fix

--- main-service/main-server/src/main/resources/schema.sql
+++ main-service/main-server/src/main/resources/schema.sql
@@ -1,7 +1,7 @@
 DROP TABLE IF EXISTS compilation_events;
-DROP TABLE IF EXISTS compilations;
 DROP TABLE IF EXISTS requests;
 DROP TABLE IF EXISTS events;
+DROP TABLE IF EXISTS compilations;
 DROP TABLE IF EXISTS categories;
 DROP TABLE IF EXISTS users;
 
@@ -21,36 +21,32 @@ CREATE TABLE events (
     title VARCHAR(120) NOT NULL,
     description TEXT,
     annotation TEXT,
-
-    category_id BIGINT NOT NULL REFERENCES categories(id),
-    initiator_id BIGINT NOT NULL REFERENCES users(id),
-
+    category_id BIGINT NOT NULL,
+    initiator_id BIGINT NOT NULL,
     event_date TIMESTAMP NOT NULL,
     paid BOOLEAN NOT NULL,
     participant_limit INTEGER NOT NULL,
     request_moderation BOOLEAN NOT NULL,
-
     state VARCHAR(20) NOT NULL,
-
     created_on TIMESTAMP NOT NULL,
     published_on TIMESTAMP,
-
     lat DOUBLE PRECISION,
     lon DOUBLE PRECISION,
-
     confirmed_requests BIGINT NOT NULL DEFAULT 0,
+
+    CONSTRAINT fk_event_category FOREIGN KEY (category_id) REFERENCES categories(id),
+    CONSTRAINT fk_event_user FOREIGN KEY (initiator_id) REFERENCES users(id)
 );
 
 CREATE TABLE requests (
     id BIGSERIAL PRIMARY KEY,
     created TIMESTAMP NOT NULL,
-
-    event_id BIGINT NOT NULL REFERENCES events(id),
-    requester_id BIGINT NOT NULL REFERENCES users(id),
-
+    event_id BIGINT NOT NULL,
+    requester_id BIGINT NOT NULL,
     status VARCHAR(20) NOT NULL,
-
-    CONSTRAINT uq_request UNIQUE (requester_id, event_id)
+    CONSTRAINT uq_request UNIQUE (requester_id, event_id),
+    CONSTRAINT fk_request_event FOREIGN KEY (event_id) REFERENCES events(id),
+    CONSTRAINT fk_request_user FOREIGN KEY (requester_id) REFERENCES users(id)
 );
 
 CREATE TABLE compilations (
@@ -60,7 +56,9 @@ CREATE TABLE compilations (
 );
 
 CREATE TABLE compilation_events (
-    compilation_id BIGINT NOT NULL REFERENCES compilations(id),
-    event_id BIGINT NOT NULL REFERENCES events(id),
-    PRIMARY KEY (compilation_id, event_id)
+    compilation_id BIGINT NOT NULL,
+    event_id BIGINT NOT NULL,
+    PRIMARY KEY (compilation_id, event_id),
+    CONSTRAINT fk_comp_event_comp FOREIGN KEY (compilation_id) REFERENCES compilations(id),
+    CONSTRAINT fk_comp_event_event FOREIGN KEY (event_id) REFERENCES events(id)
 );
