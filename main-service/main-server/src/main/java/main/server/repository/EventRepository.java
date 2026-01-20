5bc4a8677e002f0ee5b8e252648565afebcd5b6e
Dmitry Prokhorov <prokhorovds26@mail.ru>
2026-01-20 19:51:15 +0500

    fix

--- main-service/main-server/src/main/java/main/server/repository/EventRepository.java
+++ main-service/main-server/src/main/java/main/server/repository/EventRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -14,20 +15,20 @@ import java.util.Optional;
 public interface EventRepository extends JpaRepository<Event, Long> {
 
     @Query("""
-            select e from Event e
-            where e.state = :state
-            and (:text is null or lower(e.description) like lower(concat('%', :text, '%')))
-            and (:categories is null or e.category.id in :categories)
-            and (:paid is null or e.paid = :paid)
-            and e.eventDate between :start and :end
-            """)
+    select e from Event e
+    where e.state = :state
+    and (:text is null or lower(e.description) like lower(concat('%', :text, '%')))
+    and (:categories is null or e.category.id in :categories)
+    and (:paid is null or e.paid = :paid)
+    and e.eventDate between :start and :end
+""")
     Page<Event> findPublicEvents(
-            String text,
-            List<Long> categories,
-            Boolean paid,
-            LocalDateTime start,
-            LocalDateTime end,
-            EventState state,
+            @Param("text") String text,
+            @Param("categories") List<Long> categories,
+            @Param("paid") Boolean paid,
+            @Param("start") LocalDateTime start,
+            @Param("end") LocalDateTime end,
+            @Param("state") EventState state,
             Pageable pageable
     );
 
