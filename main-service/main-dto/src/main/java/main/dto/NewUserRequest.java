df696a38980941ab02acbe1e93425aef1b8faafc
Dmitry Prokhorov <prokhorovds26@mail.ru>
2026-01-22 16:04:12 +0500

    fix

--- main-service/main-dto/src/main/java/main/dto/NewUserRequest.java
+++ main-service/main-dto/src/main/java/main/dto/NewUserRequest.java
@@ -2,6 +2,7 @@ package main.dto;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -12,9 +13,11 @@ import lombok.NoArgsConstructor;
 public class NewUserRequest {
 
     @NotBlank
+    @Size(min = 2, max = 250)
     private String name;
 
     @Email
     @NotBlank
+    @Size(min = 6, max = 254)
     private String email;
 }
